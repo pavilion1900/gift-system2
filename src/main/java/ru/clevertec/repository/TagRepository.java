1f517da12b597f77a98019f61ca4cfe41589a3ce
Korolenko Sergey <pavilion1900@gmail.com>
2023-01-19 21:36:46 +0300

    format query

--- src/main/java/ru/clevertec/repository/TagRepository.java
+++ src/main/java/ru/clevertec/repository/TagRepository.java
@@ -10,19 +10,21 @@ public interface TagRepository extends JpaRepository<Tag, Integer> {
 
     Optional<Tag> findByNameIgnoreCase(String tagName);
 
-    @Query(value = "SELECT tag.id, tag.name "
-            + "FROM tag "
-            + "         JOIN certificate_tag ct ON tag.id = ct.tag_id "
-            + "         JOIN orders o ON ct.certificate_id = o.certificate_id "
-            + "WHERE o.user_id = "
-            + "      (SELECT user_id "
-            + "       FROM orders "
-            + "       GROUP BY user_id "
-            + "       ORDER BY SUM(cost) DESC "
-            + "       LIMIT 1) "
-            + "GROUP BY tag.id, tag.name "
-            + "ORDER BY COUNT(tag.id) DESC "
-            + "LIMIT 1", nativeQuery = true)
+    @Query(value = """
+            SELECT tag.id, tag.name 
+            FROM tag 
+                     JOIN certificate_tag ct ON tag.id = ct.tag_id 
+                     JOIN orders o ON ct.certificate_id = o.certificate_id 
+            WHERE o.user_id = 
+                  (SELECT user_id 
+                   FROM orders 
+                   GROUP BY user_id 
+                   ORDER BY SUM(cost) DESC 
+                   LIMIT 1) 
+            GROUP BY tag.id, tag.name 
+            ORDER BY COUNT(tag.id) DESC 
+            LIMIT 1
+            """, nativeQuery = true)
     Optional<Tag> findMostWidelyUsedTag();
 
     @Query(value = "SELECT last_value FROM tag_id_seq", nativeQuery = true)
